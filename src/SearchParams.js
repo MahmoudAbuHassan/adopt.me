f6500172bd7106366aa84f170952cb55920ac424
MahmoudAbuHassan <64367914+MahmoudAbuHassan@users.noreply.github.com>
2020-08-12 14:45:31 -0400

    declare effect dependencies

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -6,14 +6,17 @@ const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
-  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
 
   useEffect(() => {
-    updateBreeds([]);
-    updateBreed("");
+    setBreeds([]);
+    setBreed("");
 
-    pet.breeds(animal).then(({ breeds }) => {});
-  });
+    pet.breeds(animal).then(({ breeds }) => {
+      const breedStrings = breeds.map(({ name }) => name);
+      setBreeds(breedStrings);
+    }, console.error);
+  }, [animal, setBreed, setBreeds]);
 
   return (
     <div className="search-params">
